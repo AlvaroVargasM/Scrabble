9313d08250c2e8277811e9fb554377a34210c562
jonathangarcia <jonagaru1@gmail.com>
2019-04-08 00:30:55 -0600

    update

--- Server/Logic/Stack.h
+++ Server/Logic/Stack.h
@@ -4,6 +4,11 @@
 #include "../Networking/PackTile.h"
 #include "NodeS.h"
 
+/**
+ *The Pila class, has the function of generating chips with the combinations that the
+ *  client has requested, to be doing an exchange of values ​​every time there are chips
+ *  that do not work the player
+ */
 class Stack {
 private:
     NodeS *head;
